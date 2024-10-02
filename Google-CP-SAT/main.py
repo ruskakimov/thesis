56b01422cc36710f24ac678323dafec3e096b8d6
Rustem Kakimov <ruskakimov@gmail.com>
2024-10-02 04:06:00 -0400

    Setup CP SAT

---
+++ Google-CP-SAT/main.py
@@ -0,0 +1,39 @@
+from ortools.sat.python import cp_model
+
+def solve_graph(n, edges):
+    m = len(edges)
+    model = cp_model.CpModel()
+    labels = [None] * n
+
+    for i in range(n):
+        # Label for node i
+        labels[i] = model.new_int_var(0, m, f"label_{i}")
+    
+    # Labels must be unique
+    for i in range(n):
+        for j in range(i+1, n):
+            model.add(labels[i] != labels[j])
+
+    edge_weights = [None] * m
+
+    for i in range(m):
+        a, b = edges[i]
+        edge_weights[i] = model.new_int_var(1, m, f"edge_{a}_{b}")
+        model.AddAbsEquality(edge_weights[i], labels[a] - labels[b])
+    
+    # Edges must be unique
+    for i in range(m):
+        for j in range(i+1, m):
+            model.add(edge_weights[i] != edge_weights[j])
+    
+    solver = cp_model.CpSolver()
+    status = solver.solve(model)
+
+    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+        print(f'Time taken: {solver.WallTime()} seconds')
+        for i in range(n):
+            print(f"label for node {i} is {solver.value(labels[i])}")
+    else:
+        print("No solution found.")
+
+solve_graph(3, [(0,1), (1,2)])
