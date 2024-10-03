53fccb33515c2f127094850b85a8e53db88745a0
Rustem Kakimov <ruskakimov@gmail.com>
2024-10-03 00:40:36 -0400

    Generate trees

--- scripts/generate_graphs.py
+++ scripts/generate_graphs.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import networkx as nx
 
 graphs_dir = Path(__file__).resolve().parent.parent / 'graphs'
 
@@ -13,3 +14,10 @@ for i in range(10):
     n = 2 ** (i+1)
     edges = [(i, i + 1) for i in range(1, n)]
     write_graph_to_file(n, edges, 'snake')
+
+# Generate trees
+for i in range(1, 10):
+    n = 2 ** (i+1)
+    tree = nx.random_tree(n)
+    edges = list(tree.edges())
+    write_graph_to_file(n, edges, 'tree')
