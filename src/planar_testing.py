6ee9495744d6297e0540b20b6ccdb1978f54df72
Rustem Kakimov <ruskakimov@gmail.com>
2024-10-09 01:49:45 -0400

    Try schnyder planarity check

--- src/planar_testing.py
+++ src/planar_testing.py
@@ -3,76 +3,34 @@ from pysat.solvers import Solver
 import networkx as nx
 from dataset import rome_graphs
 
-def encode_planarity(vertices, edges):
+def encode_schnyder_planarity(vertices, edges):
     """
-    Encodes the planarity testing problem for a given graph into a SAT problem.
+    Encodes Schnyder realizer-based planarity checking into a SAT problem.
     
-    :param vertices: List of vertices in the graph.
-    :param edges: List of edges (tuples of vertices).
+    :param graph: A triangulated networkx graph.
     :return: CNF object representing the SAT problem.
     """
     cnf = CNF()
-    edge_vars = {}
-    ordering_vars = {}
+    
+    # Create variables for edge directions
+    edge_vars = {(u, v): [cnf.nv + 1, cnf.nv + 2, cnf.nv + 3] for u, v in edges}
+    
+    for u, v in edges:
+        d1, d2, d3 = edge_vars[(u, v)]
+        
+        # Ensure each edge (u, v) is assigned to exactly one direction
+        cnf.append([d1, d2, d3])  # At least one direction
+        cnf.append([-d1, -d2])    # Not both d1 and d2
+        cnf.append([-d1, -d3])    # Not both d1 and d3
+        cnf.append([-d2, -d3])    # Not both d2 and d3
 
-    # Generate crossing variables for each pair of edges
-    var_count = 1
-    for i in range(len(edges)):
-        for j in range(i + 1, len(edges)):
-            e1 = edges[i]
-            e2 = edges[j]
-
-            # Skip pairs of edges that share a vertex (they cannot cross)
-            if len(set(e1).intersection(e2)) > 0:
-                continue
-
-            # Variable representing whether e1 and e2 cross
-            var_name = f'c_{e1}_{e2}'
-            edge_vars[(e1, e2)] = var_count
-            cnf.append([-var_count])  # We want to find an embedding where all crossings are false
-            var_count += 1
-
-    # Generate ordering variables for edges around each vertex
-    for vertex in vertices:
-        adjacent_edges = [e for e in edges if vertex in e]
-
-        for i in range(len(adjacent_edges)):
-            for j in range(i + 1, len(adjacent_edges)):
-                e1 = adjacent_edges[i]
-                e2 = adjacent_edges[j]
-
-                # Variable representing whether e1 comes before e2 in clockwise order around the vertex
-                var_name = f'o_{e1}_{e2}_at_{vertex}'
-                ordering_vars[(e1, e2, vertex)] = var_count
-                var_count += 1
-
-    # Encode that if edges have a certain order, they cannot cross
-    for (e1, e2, vertex) in ordering_vars:
-        ordering_var = ordering_vars[(e1, e2, vertex)]
-        crossing_var = edge_vars.get((e1, e2)) or edge_vars.get((e2, e1))
-
-        if crossing_var:
-            # If the order is set, then the crossing variable must be false
-            cnf.append([-ordering_var, -crossing_var])
-
-    # Transitivity of ordering: if o(e1, e2) and o(e2, e3), then o(e1, e3)
-    for vertex in vertices:
-        adjacent_edges = [e for e in edges if vertex in e]
-
-        for i in range(len(adjacent_edges)):
-            for j in range(i + 1, len(adjacent_edges)):
-                for k in range(j + 1, len(adjacent_edges)):
-                    e1 = adjacent_edges[i]
-                    e2 = adjacent_edges[j]
-                    e3 = adjacent_edges[k]
-
-                    var1 = ordering_vars.get((e1, e2, vertex))
-                    var2 = ordering_vars.get((e2, e3, vertex))
-                    var3 = ordering_vars.get((e1, e3, vertex))
-
-                    if var1 and var2 and var3:
-                        # If (e1 before e2) and (e2 before e3), then (e1 before e3)
-                        cnf.append([-var1, -var2, var3])
+    # Constraints to ensure each vertex has exactly one outgoing edge per direction
+    for v in vertices:
+        out_edges = [edge_vars[(v, u)][k] for u in graph.neighbors(v) for k in range(3) if (v, u) in edge_vars]
+        for k in range(3):
+            # For direction k, ensure exactly one outgoing edge from each vertex
+            cnf.append(out_edges)  # At least one outgoing edge in direction k
+            # Further constraints to ensure that there is no more than one can be added with cardinality constraints
 
     return cnf
 
@@ -91,7 +49,7 @@ def verify_sat(graph):
     is_true_planar, _ = nx.check_planarity(graph)
 
     # Encode the problem as a SAT formula
-    cnf = encode_planarity(vertices, edges)
+    cnf = encode_schnyder_planarity(vertices, edges)
 
     # Use a SAT solver to check satisfiability
     solver = Solver()
