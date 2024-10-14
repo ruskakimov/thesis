e75fa1245e2cc69a4bda5d30082e08a9dd47c29b
Rustem Kakimov <ruskakimov@gmail.com>
2024-10-14 19:21:27 -0400

    Find encoding for X

--- src/helpers/_bool_to_cnf_playground.py
+++ src/helpers/_bool_to_cnf_playground.py
@@ -6,12 +6,12 @@ from sympy.logic.boolalg import to_cnf
 # formula = (Lxab & Lxbc & Lxca) | (Lxba | Lxcb | Lxac)
 
 # abcd
-Lij, Ljk, Lik = symbols('Lij Ljk Lik')
-formula = Implies(And(Lij, Ljk), Lik)
+# Lij, Ljk, Lik = symbols('Lij Ljk Lik')
+# formula = Implies(And(Lij, Ljk), Lik)
 
 # Convert to CNF
-cnf_formula = to_cnf(formula)
-print(cnf_formula)
+# cnf_formula = to_cnf(formula)
+# print(cnf_formula)
 
 # For abc
 # (-Lxab | -Lxbc | -Lxca | Lxab) &
@@ -23,3 +23,9 @@ print(cnf_formula)
 # (-Lxab | -Lxbc | -Lxcd | -Lxda | Lxbc) &
 # (-Lxab | -Lxbc | -Lxcd | -Lxda | Lxcd) &
 # (-Lxab | -Lxbc | -Lxcd | -Lxda | Lxda)
+
+a, b, c, d, e, f, x = symbols('a, b, c, d, e, f, x')
+formula = Implies((a & b) | (c & d) | (e & f), x)
+
+cnf_formula = to_cnf(formula)
+print(cnf_formula) # (x, ~a | ~b) & (x, ~c | ~d) & (x, ~e | ~f)
