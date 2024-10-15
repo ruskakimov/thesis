248f1f726cd362e15eb3ea700e5270e1fe2abaf0
Rustem Kakimov <ruskakimov@gmail.com>
2024-10-14 20:27:13 -0400

    Find CNF for planarity

--- src/helpers/_bool_to_cnf_playground.py
+++ src/helpers/_bool_to_cnf_playground.py
@@ -24,8 +24,28 @@ from sympy.logic.boolalg import to_cnf
 # (-Lxab | -Lxbc | -Lxcd | -Lxda | Lxcd) &
 # (-Lxab | -Lxbc | -Lxcd | -Lxda | Lxda)
 
-a, b, c, d, e, f, x = symbols('a, b, c, d, e, f, x')
-formula = Implies((a & b) | (c & d) | (e & f), x)
+
+# a, b, c, d, e, f, x = symbols('a, b, c, d, e, f, x')
+# formula = Implies((a & b) | (c & d) | (e & f), x)
+
+# cnf_formula = to_cnf(formula)
+# print(cnf_formula) # (x | ~a | ~b) & (x | ~c | ~d) & (x | ~e | ~f)
+
+
+Xijkl, Lij, Lji, Lkl, Llk, Lik, Lki, Lil, Lli, Ljk, Lkj, Ljl, Llj = symbols('Xijkl, Lij, Lji, Lkl, Llk, Lik, Lki, Lil, Lli, Ljk, Lkj, Ljl, Llj')
+# i k j l
+# i l j k
+# j k i l
+# j l i k
+# k i l j
+# k j l i
+# l i k j
+# l j k i
+formula = Implies(
+    Xijkl,
+    ~(Lik & Lkj & Ljl) &
+    ~(Lil & Llj & Ljk)
+)
 
 cnf_formula = to_cnf(formula)
-print(cnf_formula) # (x | ~a | ~b) & (x | ~c | ~d) & (x | ~e | ~f)
+print(cnf_formula)
