ee11b3a2e637f4c1f38658ebecdaa9e706b71321
Rustem Kakimov <ruskakimov@gmail.com>
2024-11-11 18:10:09 -0500

    Record num of vars/clauses

--- bench.sh
+++ bench.sh
@@ -4,13 +4,14 @@
 echo "n | Processing Time"
 echo "-----------------------------------------"
 
-for n in {26..28}
+for n in {2..28}
 do
 #   printf "Running for n = %d\n" "$n"
   
 #   time ./solvers/kissat-4.0.1-apple-amd64 "./cnf/grid_dag_${n}x${n}.cnf" > /dev/null
 
-  stat -f%z "./cnf/grid_dag_${n}x${n}.cnf"
+  # stat -f%z "./cnf/grid_dag_${n}x${n}.cnf"
+  head -n 1 "./cnf/grid_dag_${n}x${n}.cnf"
 
 #   echo "-----------------------------------------"
 done
