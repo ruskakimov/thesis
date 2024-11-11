d1f909b1fafa8fffe21a48d7511a415447660bb4
Rustem Kakimov <ruskakimov@gmail.com>
2024-11-11 03:34:45 -0500

    Add cnf size record

--- bench.sh
+++ bench.sh
@@ -4,11 +4,13 @@
 echo "n | Processing Time"
 echo "-----------------------------------------"
 
-for n in {21..25}
+for n in {2..25}
 do
-  printf "Running for n = %d\n" "$n"
+#   printf "Running for n = %d\n" "$n"
   
-  time ./solvers/kissat-4.0.1-apple-amd64 "./cnf/grid_dag_${n}x${n}.cnf" > /dev/null
+#   time ./solvers/kissat-4.0.1-apple-amd64 "./cnf/grid_dag_${n}x${n}.cnf" > /dev/null
 
-  echo "-----------------------------------------"
+  stat -f%z "./cnf/grid_dag_${n}x${n}.cnf"
+
+#   echo "-----------------------------------------"
 done
