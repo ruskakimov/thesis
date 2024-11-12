b6995062899bf7c388de4ac6268aff36f772b586
Rustem Kakimov <ruskakimov@gmail.com>
2024-11-12 00:27:08 -0500

    Compare v1 and v2

--- bench.sh
+++ bench.sh
@@ -7,8 +7,13 @@ echo "-----------------------------------------"
 for n in {2..20}
 do
   printf "Running for n = %d\n" "$n"
+
+  v1="./solvers/kissat-4.0.1-apple-amd64 ./cnf/grid_dag_${n}x${n}.cnf"
+  v2="./solvers/kissat-4.0.1-apple-amd64 ./cnf/grid_dag_v2_${n}x${n}.cnf"
+
+  hyperfine -r 20 --warmup 2 -i "$v1" "$v2"
   
-  time ./solvers/kissat-4.0.1-apple-amd64 --quiet "./cnf/grid_dag_v2_${n}x${n}.cnf" > /dev/null
+  # time ./solvers/kissat-4.0.1-apple-amd64 --quiet "./cnf/grid_dag_v2_${n}x${n}.cnf" > /dev/null
 
   # stat -f%z "./cnf/grid_dag_${n}x${n}.cnf"
   # head -n 1 "./cnf/grid_dag_${n}x${n}.cnf"
