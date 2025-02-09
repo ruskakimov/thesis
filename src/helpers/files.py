2ea0614face07b0a9f9ad2073640301ec9891ec1
Rustem Kakimov <ruskakimov@gmail.com>
2025-02-08 20:35:01 -0500

    Read function for random dags

--- src/helpers/files.py
+++ src/helpers/files.py
@@ -43,3 +43,14 @@ def north_graphs():
             yield G
         except Exception as e:
             print(f"Failed to read {gml_file.name}: {e}")
+
+def random_dag_graphs():
+    random_dag_dir = dataset_dir / 'random-dag'
+    
+    for file in random_dag_dir.glob('*.graphml'):
+        try:
+            G = nx.read_graphml(file)
+            G.name = file.name
+            yield G
+        except Exception as e:
+            print(f"Failed to read {file.name}: {e}")
