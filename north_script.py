33e3190ed0391db38952ffbd35379796c3bc9c44
Rustem Kakimov <ruskakimov@gmail.com>
2025-02-10 00:03:44 -0500

    Speedup histogram

--- north_script.py
+++ north_script.py
@@ -3,6 +3,7 @@ import os
 import re
 import re
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 def process_cp_result(file_path):
     results = {}
@@ -133,9 +134,9 @@ for filename, vals in data.items():
     if s1 < threshold or s2 < threshold:
         continue
 
-    if sat_result == 'SAT':
-        speedup = s1 / s2
-        speedups.append(speedup)
+    # if sat_result == 'SAT':
+    speedup = s1 / s2
+    speedups.append(speedup)
 
 print('total graphs:', len(speedups))
 
@@ -146,3 +147,51 @@ print('max speedup:', max(speedups))
 
 average_speedup = sum(speedups) / len(speedups)
 print('average speedup:', average_speedup)
+
+
+bins = [0.8, 1.2, 1.6, 2, 4, 8, 16, 32]
+fq = [0] * len(bins)
+
+for x in speedups:
+    for i, right in enumerate(bins):
+        if x < right:
+            fq[i] += 1
+            break
+
+labels = []
+
+for i, r in enumerate(bins):
+    if i == 0:
+        labels.append(f'<{r}x')
+    elif i == len(bins) - 1:
+        l = bins[i-1]
+        labels.append(f'>{l}x')
+    else:
+        l = bins[i-1]
+        labels.append(f'{l}–{r}x')
+
+assert(len(bins) == len(labels))
+
+plt.bar(labels, fq, edgecolor='black', color=['red', 'gray', 'green','green','green','green','green','green'])
+
+# plt.hist(speedups, bins=1000, edgecolor='black')  # 30 bins (one for each value)
+# plt.xlabel('Value')
+# plt.ylabel('Frequency')
+# # plt.title('Histogram of Values (0 to 30)')
+# plt.grid(axis='y', linestyle='--', alpha=0.7)
+# plt.xscale('log')
+
+plt.xticks(rotation=45, ha="right", rotation_mode='anchor', fontsize=12)
+plt.gcf().subplots_adjust(bottom=0.2)
+plt.tight_layout()
+
+legend_handles = [
+    mpatches.Patch(color="red", label="Regression (<0.8x)"),
+    mpatches.Patch(color="gray", label="Comparable (0.8x – 1.2x)"),
+    mpatches.Patch(color="green", label="Improvement (1.2x+)")
+]
+plt.legend(handles=legend_handles, loc="upper right")
+
+# Show plot
+# plt.show()
+plt.savefig("sat2_speedup_histogram.pdf")
