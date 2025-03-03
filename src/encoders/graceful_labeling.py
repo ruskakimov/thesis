2f3a380d4727c01118867fb462f2ed8cba981afb
Rustem Kakimov <ruskakimov@gmail.com>
2025-03-03 14:38:13 -0500

    Decode and test GL

--- src/encoders/graceful_labeling.py
+++ src/encoders/graceful_labeling.py
@@ -1,5 +1,14 @@
 from pysat.formula import CNF
 
+def get_variables(N, M):
+    # X_v_i - node `v` has label `i`. Range is [1, n*(m+1)].
+    X = lambda v, i: 1 + v*(M+1) + i
+
+    # Y_vw_j - edge `v,w` has label `j`. Range is [n*(m+1)+1, n*(m+1)+m*m].
+    Y = lambda vw, j: X(N-1, M) + 1 + vw*M + j
+
+    return (X, Y)
+
 def encode_graceful_labeling(graph):
     """
     SAT encodes graceful labeling.
@@ -13,11 +22,7 @@ def encode_graceful_labeling(graph):
     N = len(graph.nodes)
     M = len(graph.edges())
 
-    # X_v_i - node `v` has label `i`. Range is [1, n*(m+1)].
-    X = lambda v, i: 1 + v*(M+1) + i
-
-    # Y_vw_j - edge `v,w` has label `j`. Range is [n*(m+1)+1, n*(m+1)+m*m].
-    Y = lambda vw, j: X(N-1, M) + 1 + vw*M + j
+    X, Y = get_variables()
 
     # Constraint: Node `v` has at least one label
     for v in range(N):
@@ -52,3 +57,49 @@ def encode_graceful_labeling(graph):
                     cnf.append(clause)
     
     return cnf
+
+def decode_graceful_labeling(graph, solution):
+    if not solution:
+        return
+    
+    vertices = list(graph.nodes)
+    edges = list(graph.edges)
+    N = len(vertices)
+    M = len(edges)
+
+    X, Y = get_variables()
+
+    value_of = {}
+    for var in solution:
+        value_of[abs(var)] = var > 0
+        value_of[-abs(var)] = var < 0
+
+    node_labels = [-1] * N
+
+    for v in range(N):
+        for i in range(M+1):
+            if value_of[X(v, i)]:
+                node_labels[v] = i
+                break
+    
+    return node_labels
+
+def is_valid_graceful_labeling(graph, node_labels):
+    vertices = list(graph.nodes)
+    edges = list(graph.edges)
+
+    edge_labels = set()
+
+    for u, v in edges:
+        i = vertices.index(u)
+        j = vertices.index(v)
+
+        a = node_labels[i]
+        b = node_labels[j]
+
+        edge_label = abs(a-b)
+        if edge_label in edge_labels:
+            return False
+        edge_labels.add(edge_label)
+    
+    return True
---
+++ src/gl_test.py
@@ -0,0 +1,3 @@
+from helpers import rome_graphs
+
+print(len(list(rome_graphs())))
