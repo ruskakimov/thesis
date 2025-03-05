d927ada25e606fac82dc728e861aa66276b66916
Rustem Kakimov <ruskakimov@gmail.com>
2025-03-04 19:16:18 -0500

    dzn 100

--- src/gl_test.py
+++ src/gl_test.py
@@ -1,5 +1,6 @@
+from pathlib import Path
 from pysat.solvers import Solver
-from helpers import rome_graphs
+from helpers import rome_graphs, write_cnf, T
 from encoders import encode_graceful_labeling, decode_graceful_labeling, is_valid_graceful_labeling
 
 def solve(cnf):
@@ -7,22 +8,27 @@ def solve(cnf):
         result = solver.solve()
         model = solver.get_model() if result else None
         return (result, model)
+
+cnf_dir = Path(__file__).resolve().parent.parent / 'cnf'
     
 for G in rome_graphs():
     N = G.number_of_nodes()
     
-    if N > 10:
-        continue
+    # if N > 10:
+    #     continue
 
-    print(G.name)
+    # print(G.name)
     
+    T.start(G.name)
     cnf = encode_graceful_labeling(G)
+    write_cnf(cnf, cnf_dir / 'rome_GL' / f'gl_{G.name}.cnf')
+    T.stop(G.name)
     
-    result, solution = solve(cnf)
-    print('SAT:', result)
+    # result, solution = solve(cnf)
+    # print('SAT:', result)
     
-    node_labels = decode_graceful_labeling(G, solution)
-    print('Node labels:', node_labels)
+    # node_labels = decode_graceful_labeling(G, solution)
+    # print('Node labels:', node_labels)
 
-    print('Correct GL:', is_valid_graceful_labeling(G, node_labels))
+    # print('Correct GL:', is_valid_graceful_labeling(G, node_labels))
     print('-' * 30)
