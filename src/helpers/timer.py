c9ee4c47aedf5a00ed9940b2a5c3f24911c327a7
Rustem Kakimov <ruskakimov@gmail.com>
2025-03-16 13:38:22 -0400

    All dags 4

--- src/helpers/timer.py
+++ src/helpers/timer.py
@@ -5,17 +5,17 @@ class Timer:
         self.times = {}  # Dictionary to store start times for each label
 
     def start(self, label):
-        """Start a timer with a given label."""
-        self.times[label] = time.time()
-        print(f"'{label}' started.")
+        """Start a high-precision timer with a given label."""
+        self.times[label] = time.perf_counter()
+        # print(f"'{label}' started.")
 
     def stop(self, label):
         """Stop the timer with the given label and print the elapsed time."""
         if label not in self.times:
-            print(f"Timer '{label}' was not started.")
+            # print(f"Timer '{label}' was not started.")
             return None
-        elapsed_time = time.time() - self.times.pop(label)
-        print(f"'{label}' stopped. Elapsed time: {elapsed_time:.4f} seconds.")
+        elapsed_time = time.perf_counter() - self.times.pop(label)
+        # print(f"'{label}' stopped. Elapsed time: {elapsed_time:.9f} seconds.")  # More precision
         return elapsed_time
 
 T = Timer()
