f7dd3b20954f38cbc07c2b8c6aba60eb6df6bc6b
Rustem Kakimov <ruskakimov@gmail.com>
2025-03-17 15:15:44 -0400

    Plot 6

--- 6_node_plots.py
+++ 6_node_plots.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("all_dags_5_bench.csv")
+df = pd.read_csv("all_dags_6_bench.csv")
 
 # Compute m/n ratio
 df["m/n"] = df["m"] / df["n"]
@@ -10,7 +11,7 @@ df["m/n"] = df["m"] / df["n"]
 # Scatter plot: Time vs m/n
 def plot_scatter(df):
     plt.figure(figsize=(8, 5))
-    plt.scatter(df["m/n"], df["time(s)"], alpha=0.1, color='red')
+    plt.scatter(df["m/n"], df["time(s)"], alpha=0.01, color='red')
     plt.xlabel("m/n")
     plt.ylabel("Time (s)")
     plt.title("Scatter Plot: Time vs m/n")
@@ -21,10 +22,33 @@ def plot_scatter(df):
 
 # print(sorted(df["m/n"].unique()))
 
+# by_mn = defaultdict(list)
+
+# for index, row in df.iterrows():
+#     mn = row["m"] / row["n"]
+#     by_mn[mn].append(row["time(s)"])
+
+# mns = list(by_mn.keys())
+# mns.sort()
+# means = []
+
+# print(mns)
+
+# for mn in mns:
+#     mean = sum(by_mn[mn]) / len(by_mn[mn])
+#     means.append(mean)
+
+# print(means)
+
+# plt.plot(mns, means)
+# plt.show()
+
+man_mn = [0.0, 0.16666666666666666, 0.3333333333333333, 0.5, 0.6666666666666666, 0.8333333333333334, 1.0, 1.1666666666666667, 1.3333333333333333, 1.5, 1.6666666666666667, 1.8333333333333333, 2.0, 2.1666666666666665, 2.3333333333333335, 2.5]
+man_mean_time = [7.702e-05, 5.8549999999999987e-05, 6.029247619047618e-05, 6.554456111111111e-05, 6.900819047619047e-05, 7.25125181055836e-05, 7.749815535595505e-05, 8.215522022227527e-05, 8.931039951518962e-05, 0.00010511568484602627, 9.807410616190943e-05, 0.00010305658126682349, 0.00010795364248442969, 0.0001152129607614593, 0.000121728837777778, 0.00013142188888888898]
 
 # Assuming df is your DataFrame
-grouped_mean = df.groupby("m/n")["time(s)"].mean()
-grouped_median = df.groupby("m/n")["time(s)"].median()
+grouped_mean = df.groupby("m/n")["time(s)"].median()
+# grouped_median = df.groupby("m/n")["time(s)"].median()
 
 plt.figure(figsize=(8, 5))
 
@@ -32,15 +56,15 @@ plt.figure(figsize=(8, 5))
 # plt.scatter(df["m/n"], df["time(s)"], alpha=0.5, color="red", label="Original Data")
 
 # Scatter plot of mean points
-plt.scatter(grouped_mean.index, grouped_mean.values, color="blue", marker="o", s=80, label="Mean")
+plt.plot(grouped_mean.index, grouped_mean.values, color='red', marker='o', linestyle='-', linewidth=3)
 
 # Scatter plot of median points
-plt.scatter(grouped_median.index, grouped_median.values, color="green", marker="D", s=80, label="Median")
+# plt.scatter(grouped_median.index, grouped_median.values, color="green", marker="D", s=80, label="Median")
 
 plt.xlabel("m/n")
-plt.ylabel("Time (s)")
-plt.title("Scatter Plot: Time vs m/n with Mean & Median")
-plt.legend()
+plt.ylabel("median time (s)")
+plt.title("median time vs m/n")
+# plt.legend()
 plt.grid(True)
 
-# plt.show()
+plt.show()
