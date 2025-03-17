b3ab8a611e2be2763e0ecce31b95a43d1c3e176d
Rustem Kakimov <ruskakimov@gmail.com>
2025-03-17 19:02:44 -0400

    Plot SAT perc per m

--- 6_node_plots.py
+++ 6_node_plots.py
@@ -46,25 +46,55 @@ def plot_scatter(df):
 man_mn = [0.0, 0.16666666666666666, 0.3333333333333333, 0.5, 0.6666666666666666, 0.8333333333333334, 1.0, 1.1666666666666667, 1.3333333333333333, 1.5, 1.6666666666666667, 1.8333333333333333, 2.0, 2.1666666666666665, 2.3333333333333335, 2.5]
 man_mean_time = [7.702e-05, 5.8549999999999987e-05, 6.029247619047618e-05, 6.554456111111111e-05, 6.900819047619047e-05, 7.25125181055836e-05, 7.749815535595505e-05, 8.215522022227527e-05, 8.931039951518962e-05, 0.00010511568484602627, 9.807410616190943e-05, 0.00010305658126682349, 0.00010795364248442969, 0.0001152129607614593, 0.000121728837777778, 0.00013142188888888898]
 
-# Assuming df is your DataFrame
-grouped_mean = df.groupby("m/n")["time(s)"].median()
-# grouped_median = df.groupby("m/n")["time(s)"].median()
 
+
+
+
+
+
+
+# Group by 'm' and calculate the SAT percentage
+sat_percentage = df.groupby('m')['sat'].mean() * 100  # Mean gives the proportion of True values
+
+# Plotting
 plt.figure(figsize=(8, 5))
-
-# Scatter plot of original data
-# plt.scatter(df["m/n"], df["time(s)"], alpha=0.5, color="red", label="Original Data")
-
-# Scatter plot of mean points
-plt.plot(grouped_mean.index, grouped_mean.values, color='red', marker='o', linestyle='-', linewidth=3)
-
-# Scatter plot of median points
-# plt.scatter(grouped_median.index, grouped_median.values, color="green", marker="D", s=80, label="Median")
-
-plt.xlabel("m/n")
-plt.ylabel("median time (s)")
-plt.title("median time vs m/n")
-# plt.legend()
-plt.grid(True)
-
+plt.plot(sat_percentage.index, sat_percentage.values, color='royalblue', marker='o', linestyle='-', linewidth=3)
+plt.xlabel('m')
+plt.ylabel('Percentage of SAT (%)')
+plt.title('Percentage of SAT per m')
+# plt.ylim(0, 100)
+plt.xticks(sat_percentage.index)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
+
+
+
+
+
+
+
+
+
+
+# Assuming df is your DataFrame
+# grouped_mean = df.groupby("m/n")["time(s)"].median()
+# # grouped_median = df.groupby("m/n")["time(s)"].median()
+
+# plt.figure(figsize=(8, 5))
+
+# # Scatter plot of original data
+# # plt.scatter(df["m/n"], df["time(s)"], alpha=0.5, color="red", label="Original Data")
+
+# # Scatter plot of mean points
+# plt.plot(grouped_mean.index, grouped_mean.values, color='red', marker='o', linestyle='-', linewidth=3)
+
+# # Scatter plot of median points
+# # plt.scatter(grouped_median.index, grouped_median.values, color="green", marker="D", s=80, label="Median")
+
+# plt.xlabel("m/n")
+# plt.ylabel("median time (s)")
+# plt.title("median time vs m/n")
+# # plt.legend()
+# plt.grid(True)
+
+# plt.show()
